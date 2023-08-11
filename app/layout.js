2d0499ea1b6704211db729fd800a288e3921e64e
owusuvictorjnr <owusuvictor80@gmail.com>
2023-08-11 10:56:54 -0400

    Initial Commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,9 @@
+// 'use client'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <div className="">
+        <body className="">
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </div>
     </html>
   )
 }
